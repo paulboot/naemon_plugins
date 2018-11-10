136c578f097d3412894fc0cdfd972487a1a05b97
paulboot <paulboot@gmail.com>
2018-11-10 07:03:21 +0100

    Added full check_http support for check_v46, minor refactor check_crypto.sh

--- check_crypto.sh
+++ check_crypto.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Writen by Craig Dienger see: https://www.nagios.com/news/2018/01/monitoring-cryptocurrency-nagios/
-# Minor tweaks by Paul Boot
-
+# Minor tweaks by Paul Boot (added performance counts for graphing)
 
 #Convert crypto values
 
@@ -20,33 +19,42 @@
 # 10:20 				< 10 or > 20, (outside the range of {10 .. 20})
 # @10:20 				≥ 10 and ≤ 20, (inside the range of {10 .. 20})
 
-#Example: ./crypto.sh BTC USD "~:12000" "~:15000"
+#Example: ./check_crypto.sh BTC USD "~:12000" "~:15000"
 #Generates a warning if price is over 12,000 and critical if above 15,000
-
-result=`/usr/bin/curl -X GET "https://min-api.cryptocompare.com/data/price?fsym=$1&tsyms=$2" -m 30 -s | awk -F '[:}]' '{print $2}'` 
-
 
 source /usr/lib/nagios/plugins/utils.sh
 
-check_range $result $3
-warning=$?
-check_range $result $4
-critical=$?
+YES=0
+NO=1
+ERR=2
 
-if [ $critical -eq 0 ]
+RESULT=`/usr/bin/curl -X GET "https://min-api.cryptocompare.com/data/price?fsym=$1&tsyms=$2" -m 30 -s | awk -F '[:}]' '{print $2}'` 
+
+check_range $RESULT $3
+WARNING=$?
+check_range $RESULT $4
+CRITICAL=$?
+
+if [ $WARNING -eq $NO ]
 then
-	echo "CRITICAL $1 @ $result $2|$1_$2=$result;$3;$4;0"
-	exit 2 
+	echo "OK $1 @ $RESULT $2|$1_$2=$RESULT;$3;$4;0"
+	exit $STATE_OK
 fi
 
-if [ $warning -eq 1 ]
+if [[ $WARNING -eq $YES && $CRITICAL -eq $NO ]] 
 then
-	echo "OK $1 @ $result $2|$1_$2=$result;$3;$4;0"
-	exit 0
+	echo "WARNING $1 @ $RESULT $2|$1_$2=$RESULT;$3;$4;0"
+	exit $STATE_WARNING
 fi
 
-if [ $warning -eq 0 ] 
+if [ $CRITICAL -eq $YES ]
 then
-	echo "WARNING $1 @ $result $2|$1_$2=$result;$3;$4;0"
-	exit 1 
+	echo "CRITICAL $1 @ $RESULT $2|$1_$2=$RESULT;$3;$4;0"
+	exit $STATE_CRITICAL
+fi
+
+if [[ $WARNING -eq $ERR || $CRITICAL -eq $ERR ]]
+then
+    echo "UNKNOWN $! invalid range"
+    exit $STATE_UNKNOWN
 fi
