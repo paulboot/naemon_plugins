feb09eca6c007b13671c85e3afb252cf1d8e80ff
Paul Boot <paulboot@gmail.com>
2020-06-07 21:44:51 +0200

    check_dns LF line endings

--- check_dns.py
+++ check_dns.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-# check commands
-# check_command  check_dns!www.example.nl!cname=www1.example.nl
-# check_command  check_dns!www1.example.nl!a=192.168.1.1
+#check commands
+#check_command  check_dns!www.example.nl!cname=www1.example.nl
+#check_command  check_dns!www1.example.nl!a=192.168.1.1
 
-# define command {
-#   command_name                   check_dns
-#   command_line                   $USER1$/check_dns.py $ARG1$ $ARG2$
-# }
+#define command {
+#  command_name                   check_dns
+#  command_line                   $USER1$/check_dns.py $ARG1$ $ARG2$
+#}
 
 import optparse
 import os
@@ -17,37 +17,34 @@ import dns.rdatatype
 import dns.resolver
 import sys
 
-
-def dnscheck(domain, rdtype, expected=None, timeout=10.0):
+def dnscheck(domain, rdtype, expected=None, timeout=None):
     """
     Queries the rdtype records of the domain its authoritive nameservers and
     checks whether the received answers are all equal to the expected answer.
     """
+    if timeout is None: timeout = 10.0
     answers = resolve_authoritive(domain, rdtype, timeout)
     if not answers:
-        print("%s %s no answer" % (domain, dns.rdatatype.to_text(rdtype)))
+        print "%s %s no answer" % (domain, dns.rdatatype.to_text(rdtype))
         return False
     elif not equal_answers(answers):
-        print("%s %s different answers, expected %s" % (domain,
-                                                        dns.rdatatype.to_text(rdtype),
-                                                        list_to_text(expected)))
+        print "%s %s different answers, expected %s" % (domain,
+                dns.rdatatype.to_text(rdtype), list_to_text(expected))
         for nameserver, answer in answers.items():
             rrs = get_rrs(answer, rdtype)
-            print(" nameserver %s: %s %s" % (nameserver,
-                                             dns.rdatatype.to_text(rdtype),
-                                             list_to_text(rrs)))
+            print " nameserver %s: %s %s" % (nameserver,
+                    dns.rdatatype.to_text(rdtype), list_to_text(rrs))
         return False
     answer = answers.values()[0]
     rrs = get_rrs(answer, rdtype)
     if set(rrs) == set(expected):
-        print("%s %s %s" % (domain, dns.rdatatype.to_text(rdtype),
-                            list_to_text(rrs)))
+        print "%s %s %s" % (domain, dns.rdatatype.to_text(rdtype),
+                list_to_text(rrs))
         return True
     else:
-        print("%s %s %s, expected %s" % (domain,
-                                         dns.rdatatype.to_text(rdtype),
-                                         list_to_text(rrs),
-                                         list_to_text(expected)))
+        print "%s %s %s, expected %s" % (domain,
+                dns.rdatatype.to_text(rdtype), list_to_text(rrs),
+                list_to_text(expected))
         return False
 
 
@@ -55,20 +52,17 @@ def equal_answers(answers):
     rrsets = answers.values()
     return all(rrset == rrsets[0] for rrset in rrsets[1:]) if rrsets else True
 
-
 def get_rrs(answer, rdtype):
     rrsets = [rrset for rrset in answer if rrset.rdtype == rdtype]
     if rrsets:
         assert len(rrsets) <= 1, "Multiple %s record sets: %s" \
-            % (dns.rdatatype.to_text(rdtype), answer)
+                % (dns.rdatatype.to_text(rdtype), answer)
         return [rr.to_text().replace(' ', ':').rstrip('.') for rr in rrsets[0]]
     else:
         return []
 
-
-def list_to_text(list):
-    return ','.join(list) if list else 'EMPTY'
-
+def list_to_text(l):
+    return ','.join(l) if l else 'EMPTY'
 
 def resolve_authoritive(domain, rdtype, timeout):
     nameservers = find_nameservers(domain)
@@ -87,7 +81,6 @@ def resolve_authoritive(domain, rdtype, timeout):
             nsanswers[nsaddress] = response.answer
     return nsanswers
 
-
 def find_nameservers(domain):
     while domain:
         try:
@@ -101,14 +94,12 @@ def find_nameservers(domain):
             domain = domain[dotpos+1:]
         else:
             raise dns.resolver.NoAnswer("Error finding nameserver for %s"
-                                        % domain)
-
+                    % domain)
 
 def error(argv, msg):
     basename = os.path.basename(argv[0])
-    print("%s: error: %s" % (basename, msg), file=sys.stderr)
+    print >>sys.stderr, "%s: error: %s" % (basename, msg)
     sys.exit(2)
-
 
 class ExampleHelpFormatter(optparse.IndentedHelpFormatter):
     def format_epilog(self, epilog):
@@ -116,7 +107,6 @@ class ExampleHelpFormatter(optparse.IndentedHelpFormatter):
             return "\n" + epilog
         else:
             return ""
-
 
 def main(argv):
     # Set-up command line parser
@@ -131,9 +121,9 @@ Examples:
     parser.set_usage(usage)
     parser.epilog = epilog.replace("%prog", parser.get_prog_name())
     parser.add_option("-t", "--timeout", dest="timeout",
-                      type="float", default=10.0,
-                      help="set the DNS query timeout to TIMEOUT seconds",
-                      metavar="TIMEOUT")
+            type="float", default=10.0,
+            help="set the DNS query timeout to TIMEOUT seconds",
+            metavar="TIMEOUT")
 
     # Parse command line
     (options, args) = parser.parse_args()
@@ -155,11 +145,10 @@ Examples:
     # Execute DNS check
     try:
         return 0 if dnscheck(domain, rdtype, expected, options.timeout) else 1
-    except dns.resolver.NoAnswer as e:
+    except dns.resolver.NoAnswer, e:
         error(argv, e.message)
-    except dns.exception.Timeout:
+    except dns.exception.Timeout, e:
         error(argv, "timeout waiting for nameserver")
-
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
