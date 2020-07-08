e7d588c4fe310eb88f009f6010ba49d47159cd2d
Paul Boot <paulboot@gmail.com>
2020-07-08 22:26:18 +0200

    Fixed formatting tests

--- check_gcalendar_test.py
+++ check_gcalendar_test.py
@@ -12,6 +12,9 @@ class TestCalendar_eventgethours(unittest.TestCase):
     def test_validhours(self):
         self.assertAlmostEqual(c._eventgethours('Uren: NP8 Km: K MKBoZ K'), 8)
         self.assertAlmostEqual(c._eventgethours('Uren: NP4 P4 Km: K MKBoZ K'), 8)
+        self.assertAlmostEqual(c._eventgethours('Uren: NP10 P4 Km: K MKBoZ K'), 14)
+        self.assertAlmostEqual(c._eventgethours('Uren: NP10 P14 Km: K MKBoZ K'), 24)
 
     def test_invalidhours(self):
-        self.assertRaises(AttributeError, c._eventgethours, 'Urn: NP4P4 Km: K MKBoZ K')
+        self.assertRaises(ValueError, c._eventgethours, 'Urn: NP4 P4 Km: K MKBoZ K')
+        self.assertRaises(ValueError, c._eventgethours, 'Uren: NP4P4 Km: K MKBoZ K')
